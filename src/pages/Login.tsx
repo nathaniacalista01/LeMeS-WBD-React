3f7b33110e3568b487fc0203677b2b1b7cdefda1
ferindyaa <13521161@std.stei.itb.ac.id>
2023-11-04 21:57:48 +0700

    feat: login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,40 @@
 import React from 'react'
-import {Container, Heading} from "@chakra-ui/react"
+// import {Container, Heading} from "@chakra-ui/react"
 
 const Login = () => {
   return (
-    <Container>
-        <Heading>
-            Login page
-        </Heading>
-    </Container>
+    <div className="container">
+      <div className="w-50">
+        <form className="card p-4 mt-4">
+          <h1>Login Form</h1>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
