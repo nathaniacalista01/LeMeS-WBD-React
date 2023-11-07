75dfc7ef0c3a49fa8d3a7b6149bd0f6d34ee8893
akhmadst1 <13521164@mahasiswa.itb.ac.id>
2023-11-07 16:47:33 +0700

    feat: new login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,87 +1,108 @@
+import React from "react";
 import {
-  Text,
-  Box,
   Flex,
-  Stack,
   Button,
-  Image,
   FormControl,
+  FormLabel,
+  Heading,
   Input,
-  InputGroup,
-  useColorModeValue,
+  Text,
   Container,
 } from "@chakra-ui/react";
-
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
 function Login() {
+  const titleColor = "purple.500";
+  const textColor = "black";
   return (
-    <>
-    <Container
-    justifyContent={"center"}>
+    <Container display={"flex"} flexDir={"column"}>
       <Flex
-        minH={"100vh"}
-        align={"center"}
-        justify={"center"}
-        textColor={"black"}
-        bg={useColorModeValue("#f2f2f2", "gray.800")}
-        >
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={8} px={6}>
-          <Stack align={"center"}>
-            <Image src="premiumlogo.png" alt="logo" mb={8} />
-            <Text fontSize={"lg"} color={"black"} fontWeight={"bold"}>
-              Sign In
+        direction='column'
+        alignSelf='center'
+        justifySelf='center'
+        mt="3rem"
+        overflow='hidden'>
+        <Flex alignItems='center' justifyContent='center' mb='60px'>
+          <Flex
+            direction='column'
+            w='445px'
+            background='transparent'
+            borderRadius='15px'
+            p='40px'
+            mx={{ base: "100px" }}
+            bg={"#f2f2f2"}
+            boxShadow='0 20px 27px 0 rgb(0 0 0 / 15%)'>
+            <Heading color={titleColor} fontSize='32px' mb='10px'>
+              SIGN IN
+            </Heading>
+            <Text
+              mb='36px'
+              ms='4px'
+              color={"gray.600"}
+              fontWeight='bold'
+              fontSize='14px'>
+              Welcome to LeMeS Premium!
             </Text>
-          </Stack>
-
-          <Box>
-            <Stack spacing={4}>
-              <FormControl id="username" isRequired>
-                <Input
-                  type="text"
-                  bg="white"
-                  placeholder="Username"
-                  border={"none"}
-                  />
-              </FormControl>
-
-              <FormControl id="password" isRequired>
-                <InputGroup>
-                  <Input
-                    type="password"
-                    bg="white"
-                    placeholder="Password"
-                    border={"none"}
-                    />
-                </InputGroup>
-              </FormControl>
-
-              <Stack spacing={10} pt={2}>
-                <Button
-                  borderRadius={"22px"}
-                  size="lg"
-                  bg={"#9d4bff"}
-                  color={"white"}
-                  _hover={{ bg: "#23004d" }}
-                  >
-                  Login
-                </Button>
-              </Stack>
-
-              <Stack pt={6}>
-                <Text align={"center"}>
-                  Don't have an account?{" "}
-                    <Link to={{ pathname: "/register" }} style={{ color: "#9d4bff" }}>
-                      Sign Up
-                    </Link>
-                </Text>
-              </Stack>
-            </Stack>
-          </Box>
-        </Stack>
+            <FormControl>
+              <FormLabel ms='4px' fontSize='sm' fontWeight='bold'>
+                Username
+              </FormLabel>
+              <Input
+                bg="white"
+                borderRadius='15px'
+                mb='24px'
+                fontSize='sm'
+                type='text'
+                placeholder='Your username'
+                size='lg'
+              />
+              <FormLabel ms='4px' fontSize='sm' fontWeight='bold'>
+                Password
+              </FormLabel>
+              <Input
+                bg="white"
+                borderRadius='15px'
+                mb='36px'
+                fontSize='sm'
+                type='password'
+                placeholder='Your password'
+                size='lg'
+              />
+              <Button
+                fontSize='16px'
+                type='submit'
+                bg='purple.500'
+                w='100%'
+                h='45'
+                mb='20px'
+                color='white'
+                mt='20px'
+                _hover={{
+                  bg: "purple.200",
+                }}
+                _active={{
+                  bg: "purple.400",
+                }}>
+                Login
+              </Button>
+            </FormControl>
+            <Flex
+              flexDirection='column'
+              justifyContent='center'
+              alignItems='center'
+              maxW='100%'
+              mt='0px'>
+              <Text color={textColor} fontWeight='medium'>
+                Don't have an account?
+                <RouterLink to="/register" style={{color: "#564c95", marginLeft: '5px', fontWeight: 'bold'}}>
+                  Sign Up
+                </RouterLink>
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
       </Flex>
-      </Container>
-    </>
+    </Container >
   );
 }
 
