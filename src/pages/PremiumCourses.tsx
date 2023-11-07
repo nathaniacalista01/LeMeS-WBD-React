735a96892fa91b69233eb482440b04299e58d83b
akhmadst1 <13521164@mahasiswa.itb.ac.id>
2023-11-07 14:20:47 +0700

    style: premium courses admin view

--- src/pages/PremiumCourses.tsx
+++ src/pages/PremiumCourses.tsx
@@ -13,6 +13,7 @@ import {
     Flex,
     TableContainer,
     Icon,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import {
     BiSolidTrash,
@@ -37,34 +38,33 @@ const CoursesList = () => {
         { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
         { course_id: 4, title: 'Course 4', description: 'Course4', course_password: 12347, release_date: "1-1-2023" },
         { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'xcCourse5', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Czxczxourse5', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Courzxczxce5czxc', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Courzxcse5czxzxczxvzxczxc', course_password: 12347, release_date: "1-1-2023" },
-        { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'xcCourse5', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Czxczxourse5', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Courzxczxce5czxc', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Course5', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Courzxcse5czxzxczxvzxczxc', course_password: 12347, release_date: "1-1-2023" },
+        // { course_id: 5, title: 'Course 5', description: 'Courszxcze5zxczxczxczxczx', course_password: 12347, release_date: "1-1-2023" },
     ]);
 
     return (
         <Container
-            minW={"70vw"}
-            minH={"100vh"}
-        >
-            <Flex
-                minH={"100vh"}
-                align={"center"}
-                justify={"center"}
-                textColor={"black"}
-                width={"auto"}
-                py="6"
-                ml="250"
-            >
-                <Box>
-                    <Box display="flex" justifyContent="space-between" alignItems="center">
+            overflow="auto"
+            maxW={"100vw"}
+            maxH={"100vh"}>
+            <Flex alignItems='center' justifyContent='center' mb='20px' mt='50px'>
+                <Flex
+                    direction='column'
+                    background='transparent'
+                    borderRadius='15px'
+                    p='30px'
+                    // mx={{ base: "100px" }}
+                    bg={useColorModeValue("#f2f2f2", "gray.700")}
+                    boxShadow='0 20px 27px 0 rgb(0 0 0 / 15%)'>
+                    <Box display="flex" justifyContent="space-between" alignItems="center" mt="-5">
                         <Heading as="h1">
                             Premium Courses
                         </Heading>
@@ -76,49 +76,47 @@ const CoursesList = () => {
                             </Button>
                         </Box>
                     </Box>
-
-                    <Box>
-                        <TableContainer width="65vw">
-                            {/* HEADER TABLE */}
-                            <Table variant="striped" colorScheme="purple">
-                                <Thead>
-                                    <Tr bg="#564c95" fontWeight={"bold"}>
-                                        <Th color={"white"}>CourseID</Th>
-                                        <Th color={"white"}>Title</Th>
-                                        <Th color={"white"}>Description</Th>
-                                        <Th color={"white"}>Code</Th>
-                                        <Th color={"white"}>Release Date</Th>
-                                        <Th color={"white"}>Action</Th>
+                    <TableContainer width="80vw">
+                        {/* HEADER TABLE */}
+                        <Table variant="striped" colorScheme="purple">
+                            <Thead>
+                                <Tr bg="#564c95" fontWeight={"bold"}>
+                                    <Th color={"white"}>CourseID</Th>
+                                    <Th color={"white"}>Title</Th>
+                                    <Th color={"white"}>Description</Th>
+                                    <Th color={"white"}>Release Date</Th>
+                                    <Th color={"white"}>Action</Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                {/* ITERATE HERE LOOPING DATABASE TO FILL TABLE */}
+                                {courses.map((item, index) => (
+                                    <Tr
+                                        key={index}
+                                        _even={{ bg: 'white' }}
+                                    >
+                                        <Td>{item.course_id}</Td>
+                                        <Td>{item.title}</Td>
+                                        <Td>{item.description}</Td>
+                                        <Td>{item.release_date}</Td>
+                                        <Td>
+                                            <Link to={`/admin/edit-course/${item.course_id}`} style={{ textDecoration: 'none' }}>
+                                                <Icon as={BiSolidEdit} fontSize={"24"} color={"#564c95"} _hover={{ color: "green" }}></Icon>
+                                            </Link>
+                                            <Link to={`/admin/delete-course/${item.course_id}`} style={{ textDecoration: 'none' }}>
+                                                <Icon as={BiSolidTrash} fontSize={"24"} color={"#564c95"} _hover={{ color: "red" }}></Icon>
+                                            </Link>
+                                        </Td>
                                     </Tr>
-                                </Thead>
-                                <Tbody>
-                                    {/* ITERATE HERE LOOPING DATABASE TO FILL TABLE */}
-                                    {courses.map((item) => (
-                                        <Tr key={item.course_id}>
-                                            <Td>{item.course_id}</Td>
-                                            <Td>{item.title}</Td>
-                                            <Td>{item.description}</Td>
-                                            <Td>{item.course_password}</Td>
-                                            <Td>{item.release_date}</Td>
-                                            <Td>
-                                                <Link to={`/admin/editcourse/${item.course_id}`} style={{ textDecoration: 'none' }}>
-                                                    <Icon as={BiSolidEdit} fontSize={"24"} color={"#564c95"} _hover={{ color: "green" }}></Icon>
-                                                </Link>
-                                                <Link to={`/admin/deletecourse/${item.course_id}`} style={{ textDecoration: 'none' }}>
-                                                    <Icon as={BiSolidTrash} fontSize={"24"} color={"#564c95"} _hover={{ color: "red" }}></Icon>
-                                                </Link>
-                                            </Td>
-                                        </Tr>
-                                    ))}
-                                </Tbody>
-                            </Table>
-                            {/* Pagination component */}
-                            {/* Replace with your pagination component */}
-                        </TableContainer>
-                    </Box>
-                </Box>
+                                ))}
+                            </Tbody>
+                        </Table>
+                        {/* Pagination component */}
+                        {/* Replace with your pagination component */}
+                    </TableContainer>
+                </Flex>
             </Flex>
-        </Container>
+        </Container >
     );
 };
 
