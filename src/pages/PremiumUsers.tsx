35934e7e952b35a3c125c7dd6706f99fd814d821
akhmadst1 <13521164@mahasiswa.itb.ac.id>
2023-11-07 14:20:57 +0700

    feat: premium users admin view

--- src/pages/PremiumUsers.tsx
+++ src/pages/PremiumUsers.tsx
@@ -4,6 +4,7 @@ import {
     Heading,
     Button,
     Table,
+    Text,
     Thead,
     Tbody,
     Tr,
@@ -13,6 +14,7 @@ import {
     Flex,
     TableContainer,
     Icon,
+    useColorModeValue,
 } from '@chakra-ui/react';
 import {
     BiSolidTrash,
@@ -34,80 +36,75 @@ const UsersList = () => {
         { user_id: 5, username: 'User12', fullname: 'Courszxcze5zxczxczxczxczx', role: "admin" },
         { user_id: 2, username: 'User4', fullname: 'user2', role: "student" },
         { user_id: 3, username: 'User7', fullname: 'user3', role: "teacher" },
-        { user_id: 4, username: 'user 4', fullname: 'user4', role: "admin" },
-        { user_id: 5, username: 'user 5', fullname: 'user5', role: "teacher" },
-        { user_id: 5, username: 'user 5', fullname: 'xcuser5', role: "student" },
-        { user_id: 5, username: 'user 5', fullname: 'Courszxcze5zxczxczxczxczx', role: "student" }
     ]);
 
     return (
         <Container
-            minW={"70vw"}
-            minH={"100vh"}
-        >
-            <Flex
-                minH={"100vh"}
-                align={"center"}
-                justify={"center"}
-                textColor={"black"}
-                width={"auto"}
-                py="6"
-                ml="250"
-            >
-                <Box>
-                    <Box display="flex" justifyContent="space-between" alignItems="center">
+            overflow="auto"
+            maxW={"100vw"}
+            maxH={"100vh"}>
+            <Flex alignItems='center' justifyContent='center' mb='20px' mt='50px'>
+                <Flex
+                    direction='column'
+                    background='transparent'
+                    borderRadius='15px'
+                    p='30px'
+                    // mx={{ base: "100px" }}
+                    bg={useColorModeValue("#f2f2f2", "gray.700")}
+                    boxShadow='0 20px 27px 0 rgb(0 0 0 / 15%)'>
+                    <Box display="flex" justifyContent="space-between" alignItems="center" mt="-5">
                         <Heading as="h1">
                             Premium Users
                         </Heading>
                         <Box>
                             <Button bg="#9d4bff" textColor="white" _hover={{ bg: "#23004d" }} my="5">
-                                <Link to="/admin/adduser">
-                                    Add User
+                                <Link to="/admin/addcourse">
+                                    Add Users
                                 </Link>
                             </Button>
                         </Box>
                     </Box>
-
-                    <Box>
-                        <TableContainer width="65vw">
-                            {/* HEADER TABLE */}
-                            <Table variant="striped" colorScheme="purple">
-                                <Thead>
-                                    <Tr bg="#564c95" fontWeight={"bold"}>
-                                        <Th color={"white"}>UserID</Th>
-                                        <Th color={"white"}>Username</Th>
-                                        <Th color={"white"}>Fullname</Th>
-                                        <Th color={"white"}>Role</Th>
-                                        <Th color={"white"}>Action</Th>
+                    <TableContainer width="80vw">
+                        {/* HEADER TABLE */}
+                        <Table variant="striped" colorScheme="purple">
+                            <Thead>
+                                <Tr bg="#564c95" fontWeight={"bold"}>
+                                    <Th color={"white"}>UserID</Th>
+                                    <Th color={"white"}>Username</Th>
+                                    <Th color={"white"}>Fullname</Th>
+                                    <Th color={"white"}>Role</Th>
+                                    <Th color={"white"}>Action</Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
+                                {/* ITERATE HERE LOOPING DATABASE TO FILL TABLE */}
+                                {users.map((item, index) => (
+                                    <Tr
+                                        key={index}
+                                        _even={{ bg: 'white' }}
+                                    >
+                                        <Td>{item.user_id}</Td>
+                                        <Td>{item.username}</Td>
+                                        <Td>{item.fullname}</Td>
+                                        <Td>{item.role}</Td>
+                                        <Td>
+                                            <Link to={`/admin/edituser/${item.user_id}`} style={{ textDecoration: 'none' }}>
+                                                <Icon as={BiSolidEdit} fontSize={"24"} color={"#564c95"} _hover={{ color: "green" }}></Icon>
+                                            </Link>
+                                            <Link to={`/admin/deleteuser/${item.user_id}`} style={{ textDecoration: 'none' }}>
+                                                <Icon as={BiSolidTrash} fontSize={"24"} color={"#564c95"} _hover={{ color: "red" }}></Icon>
+                                            </Link>
+                                        </Td>
                                     </Tr>
-                                </Thead>
-                                <Tbody>
-                                    {/* ITERATE HERE LOOPING DATABASE TO FILL TABLE */}
-                                    {users.map((item) => (
-                                        <Tr key={item.user_id}>
-                                            <Td>{item.user_id}</Td>
-                                            <Td>{item.username}</Td>
-                                            <Td>{item.fullname}</Td>
-                                            <Td>{item.role}</Td>
-                                            <Td>
-                                                <Link to={`/admin/edituser/${item.user_id}`} style={{ textDecoration: 'none' }}>
-                                                    <Icon as={BiSolidEdit} fontSize={"24"} color={"#564c95"} _hover={{ color: "green" }}></Icon>
-                                                </Link>
-                                                <Link to={`/admin/deleteuser/${item.user_id}`} style={{ textDecoration: 'none' }}>
-                                                    <Icon as={BiSolidTrash} fontSize={"24"} color={"#564c95"} _hover={{ color: "red" }}></Icon>
-                                                </Link>
-                                            </Td>
-                                        </Tr>
-                                    ))}
-                                </Tbody>
-                            </Table>
-                            {/* Pagination component */}
-                            {/* Replace with your pagination component */}
-                        </TableContainer>
-                    </Box>
-                </Box>
+                                ))}
+                            </Tbody>
+                        </Table>
+                        {/* Pagination component */}
+                        {/* Replace with your pagination component */}
+                    </TableContainer>
+                </Flex>
             </Flex>
-        </Container>
+        </Container >
     );
 };
 
