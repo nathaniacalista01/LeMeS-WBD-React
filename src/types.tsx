f92d23e27bb107f13f03e33fa64bc795ffc6694e
akhmadst1 <13521164@mahasiswa.itb.ac.id>
2023-11-15 03:13:53 +0700

    feat: list modules of a course

--- src/types.tsx
+++ src/types.tsx
@@ -5,3 +5,19 @@ export type Courses = {
     image_path: String;
     release_date: string;
 }
+
+export type Modules = {
+    id: number;
+    title: string;
+    description: string;
+    course_id: number;
+  };
+
+export type Materials = {
+    id: number;
+    title: string;
+    description: string;
+    source_type: string;
+    material_path: string;
+  };
+
